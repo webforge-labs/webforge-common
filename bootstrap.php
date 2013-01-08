c0a3c9efb8b2ecc2c8d60f21b30dd0fd3d4b26df
Philipp Scheit <p.scheit@ps-webforge.com>
2013-01-08 13:25:20 +0100

    remove psc dependency in bootstrap update phpunit xml remove dependency to webforge in composer commit lockfile

--- bootstrap.php
+++ bootstrap.php
@@ -9,13 +9,5 @@ $ds = DIRECTORY_SEPARATOR;
 // autoload project dependencies and self autoloading for the library
 require_once __DIR__.$ds.'vendor'.$ds.'autoload.php';
 
-if (file_exists($cmsBootstrap = getenv('PSC_CMS').'bootstrap.php')) {
-  require_once $cmsBootstrap;
-}
-
-if (!class_exists('Psc\PSC', FALSE)) {
-  require_once __DIR__.$ds.'bin'.$ds.'psc-cms.phar.gz';
-}
-
-return $GLOBALS['env']['root'] = new \Psc\System\Dir(__DIR__.DIRECTORY_SEPARATOR);
+return $GLOBALS['env']['root'] = new \Webforge\Common\System\Dir(__DIR__.DIRECTORY_SEPARATOR);
 ?>
