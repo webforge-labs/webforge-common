0ccbc450e8bd22200ee598726fe5cc7c2a5af2b1
Philipp Scheit <p.scheit@ps-webforge.com>
2013-06-03 10:11:07 +0200

    change signature for deprecation for ability to use __FUNCTION__

--- lib/Webforge/Common/DeprecatedException.php
+++ lib/Webforge/Common/DeprecatedException.php
@@ -4,11 +4,11 @@ namespace Webforge\Common;
 
 class DeprecatedException extends Exception {
 
-  public static function fromClassMethod($class, $method) {
-    return new static(sprintf('The classmethod %s::%s is deprecated', $class, $method));
+  public static function fromMethod($method) {
+    return new static(sprintf('The function %s is deprecated', $method));
   }
 
-  public static function fromClassMethodParam($class, $method, $paramNum, $msg) {
-    return new static(sprintf('The parameter #%d from classmethod %s::%s is deprecated: %s', $paramNum, $class, $method, $msg));
+  public static function fromMethodParam($method, $paramNum, $msg) {
+    return new static(sprintf('The parameter #%d from function %s is deprecated: %s', $paramNum, $method, $msg));
   }
 }
