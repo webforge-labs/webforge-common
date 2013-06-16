a02db3421feba0b5b0a0fa1e963447b48c8fa602
Philipp Scheit <p.scheit@ps-webforge.com>
2013-06-16 11:15:32 +0200

    add a very simple Session Interface for other libraries add a Session Mock with implements the Session Interface with no calls to session_ from php

---
+++ lib/Webforge/Common/Mock/Session.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Webforge\Common\Mock;
+
+use Webforge\Common\DataStructure\KeysMap;
+
+class Session implements \Webforge\Common\Session {
+
+  protected $keysMap;
+
+  protected $init;
+  
+  public function init() {
+    if (!$this->init) {
+      
+      $this->keysMap = new KeysMap();
+    
+      $this->init = TRUE;
+    }
+    return $this;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function get() {
+    return $this->keysMap->get(func_get_args(), $default = NULL);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function set() {
+    $args = func_get_args();
+    $value = array_pop($args);
+
+    $this->keysMap->set($args, $value);
+    return $this;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getKeysMap() {
+    return $this->keysMap;
+  }
+
+  public function destroy() {
+    $this->keysMap->reset();
+    return $this;
+  }
+}
---
+++ tests/Webforge/Common/Mock/SessionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Webforge\Common\Mock;
+
+class SessionTest extends \Webforge\Code\Test\Base {
+
+  protected $session;
+  
+  public function setUp() {
+    $this->chainClass = 'Webforge\\Common\\Mock\\Session';
+    parent::setUp();
+
+    $this->session = new Session();
+    $this->session->init();
+  }
+
+  public function testInitIsSaveToCallInCLI() {
+    $this->session->init();
+
+    $this->assertInternalType('array', $this->session->getKeysMap()->toArray());
+  }
+
+  public function testReturnsNULLForUndefinedKeys() {
+    $this->session->set('some', 'value');
+
+    $this->assertNull($this->session->get('some','undefined'));
+  }
+
+  public function testReturnsTheValueOfAsettedKey() {
+    $this->session->set('some', 'value');
+
+    $this->assertEquals('value', $this->session->get('some'));
+  }
+
+  public function testUnwrapsKeyMapForOtherDirtyTests() {
+    $this->assertInstanceOf('Webforge\Common\DataStructure\KeysMap', $this->session->getKeysMap());
+  }
+}
