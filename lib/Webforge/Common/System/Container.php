862ddb57220cb7da31a8c8c691fa6e35e9ae5add
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-10 11:09:58 +0200

    add a ContainerConfiguration Interface which is the required parameter for System Container

--- lib/Webforge/Common/System/Container.php
+++ lib/Webforge/Common/System/Container.php
@@ -12,6 +12,15 @@ class Container {
   protected $system;
   
   /**
+   * @var Webforge\Common\System\ExecutableFinder
+   */
+  protected $executableFinder;
+  
+
+  public function __construct(ContainerConfiguration $configuration) {
+    $this->configuration = $configuration;
+  }
+  /**
    * @return Webforge\Common\System\System
    */
   public function getSystem() {
@@ -27,6 +36,25 @@ class Container {
    */
   public function injectSystem(System $system) {
     $this->system = $system;
+    return $this;
+  }
+
+  /**
+   * @return Webforge\Common\System\ExecutableFinder
+   */
+  public function getExecutableFinder() {
+    if (!isset($this->executableFinder)) {
+      $this->executableFinder = new ExecutableFinder($this->configuration->forExecutableFinder());
+    }
+    return $this->executableFinder;
+  }
+
+  /**
+   * @param Webforge\Common\System\ExecutableFinder $executableFinder
+   * @chainable
+   */
+  public function injectExecutableFinder(ExecutableFinder $executableFinder) {
+    $this->executableFinder = $executableFinder;
     return $this;
   }
 }
--- tests/Webforge/Common/System/ContainerTest.php
+++ tests/Webforge/Common/System/ContainerTest.php
@@ -2,14 +2,18 @@
 
 namespace Webforge\Common\System;
 
+use Mockery as m;
+
 class ContainerTest extends \Webforge\Code\Test\Base {
   
   public function setUp() {
     $this->chainClass = 'Webforge\\Common\\System\\Container';
     parent::setUp();
 
-    $this->container = new Container();
-    $this->systemInterface = 'Webforge\Common\System\System';
+    $this->config = m::mock(__NAMESPACE__.'\ContainerConfiguration');
+    $this->container = new Container($this->config);
+    
+    $this->systemInterface = __NAMESPACE__.'\System';
   }
 
   public function testContainerReturnsASystemImplementation() {
@@ -21,5 +25,24 @@ class ContainerTest extends \Webforge\Code\Test\Base {
 
     $this->container->injectSystem($system);
     $this->assertSame($system, $this->container->getSystem());
+  }
+
+  public function testReturnsAConfiguredExecutableFinder() {
+    $this->expectExecutableConfiguration();
+
+    $this->assertInstanceOf(__NAMESPACE__.'\ExecutableFinder', $finder = $this->container->getExecutableFinder());
+
+    $this->assertEquals(
+      (string) $this->someBin, 
+      (string) $finder->getExecutable('test-bin')
+    );
+  }
+
+  protected function expectExecutableConfiguration() {
+    $this->someBin = $this->getFile('fakeExecutableRar');
+    $this->config
+      ->shouldReceive('forExecutableFinder')
+      ->once()
+      ->andReturn(array('test-bin'=>(string) $this->someBin));
   }
 }
